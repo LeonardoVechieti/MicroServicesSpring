e34866afc9229fa07293f3c16e00c22585e29ac4
LeonardoVechieti <leonardovechieti@outlook.com>
2023-10-24 02:29:49 -0300

    feat: create users

---
+++ user/src/main/java/com/microservices/user/models/UserModel.java
@@ -0,0 +1,26 @@
+package com.microservices.user.models;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+//Lombok
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "TB_USERS")
+public class UserModel  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+}
