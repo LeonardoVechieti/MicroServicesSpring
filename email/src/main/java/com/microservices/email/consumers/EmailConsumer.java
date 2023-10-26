7669af13daa65d8b3fb62d8e882de4bcfe994da2
LeonardoVechieti <leonardovechieti@outlook.com>
2023-10-26 01:37:22 -0300

    feat: consumer email

--- email/src/main/java/com/microservices/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/microservices/email/consumers/EmailConsumer.java
@@ -1,15 +1,26 @@
 package com.microservices.email.consumers;
 
 import com.microservices.email.dtos.EmailRecordDTO;
+import com.microservices.email.models.EmailModel;
+import com.microservices.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailConsumer {
+
+    final EmailService emailService;
+
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDTO emailRecordDTO) {
-        System.out.println(emailRecordDTO.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDTO, emailModel);
+        emailService.sendEmail(emailModel);
     }
 }
 
